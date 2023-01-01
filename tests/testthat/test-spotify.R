c09072fd05804e485659422bb8d033205ba349d2
Judith Bourque <59173478+judith-bourque@users.noreply.github.com>
2023-01-01 17:05:37 -0500

    Add Spotify data import test

--- tests/testthat/test-spotify.R
+++ tests/testthat/test-spotify.R
@@ -1,3 +1,8 @@
 test_that("Spotify authorization works", {
   expect_success(expect_type(spotifyr::get_spotify_access_token(), "character"))
 })
+
+test_that("Spotify data import works", {
+  expect_success(expect_type(get_swift_spotify(), "list"))
+  expect_success(expect_type(get_west_spotify(), "list"))
+})
