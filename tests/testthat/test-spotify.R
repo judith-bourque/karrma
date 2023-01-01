5a5998d99ee60a3d53dcc2c628845e44364d488b
Judith Bourque <59173478+judith-bourque@users.noreply.github.com>
2023-01-01 17:13:08 -0500

    Add test for tidy Spotify

--- tests/testthat/test-spotify.R
+++ tests/testthat/test-spotify.R
@@ -6,3 +6,8 @@ test_that("Spotify data import works", {
   expect_success(expect_type(get_swift_spotify(), "list"))
   expect_success(expect_type(get_west_spotify(), "list"))
 })
+
+test_that("Tidy Spotify data works", {
+  expect_success(expect_type(get_spotify(), "list"))
+  expect_success(expect_length(get_spotify(), 3))
+})
