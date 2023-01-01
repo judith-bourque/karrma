006874322755104212a379fd8b0608c5746a8272
Judith Bourque <59173478+judith-bourque@users.noreply.github.com>
2023-01-01 16:21:45 -0500

    Add spotifyr dependencies

---
+++ R/karrma-package.R
@@ -0,0 +1,8 @@
+#' @keywords internal
+"_PACKAGE"
+
+## usethis namespace: start
+#' @importFrom spotifyr get_artist
+#' @importFrom spotifyr get_spotify_access_token
+## usethis namespace: end
+NULL
