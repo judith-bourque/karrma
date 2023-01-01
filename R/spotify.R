c53b278a58ac4622e7668e8cd97a79587382c181
Judith Bourque <59173478+judith-bourque@users.noreply.github.com>
2023-01-01 16:13:25 -0500

    Add West Spotify data

--- R/spotify.R
+++ R/spotify.R
@@ -1,10 +1,15 @@
-#' Get Spotify data for Taylor Swift
+#' Get Spotify data for Taylor Swift and Kanye West
 #'
 #' @return List object.
 #' @export
 #'
 #' @examples
 #' data <- get_swift_spotify()
+#' data <- get_west_spotify()
 get_swift_spotify <- function() {
   spotifyr::get_artist("06HL4z0CvFAxyc27GXpf02?si=_y8VtBWVQ-uiuaOF5tJphw", authorization = spotifyr::get_spotify_access_token())
+}
+#' @export
+get_west_spotify <- function() {
+  spotifyr::get_artist("5K4W6rqBFWDnAN6FQUkS6x?si=1porkVNwRaeBFt-Q0WGHQw", authorization = spotifyr::get_spotify_access_token())
 }
