fc4fae25030414ea60e049b5a21388611a379d8b
Judith Bourque <59173478+judith-bourque@users.noreply.github.com>
2023-01-01 16:26:44 -0500

    Combine documentation for Spotify data

--- R/spotify.R
+++ R/spotify.R
@@ -1,15 +1,19 @@
 #' Get Spotify data for Taylor Swift and Kanye West
 #'
 #' @return List object.
-#' @export
 #'
 #' @examples
 #' data <- get_swift_spotify()
 #' data <- get_west_spotify()
+#'
+
+#' @export
+#' @rdname get_spotify
 get_swift_spotify <- function() {
   spotifyr::get_artist("06HL4z0CvFAxyc27GXpf02?si=_y8VtBWVQ-uiuaOF5tJphw", authorization = spotifyr::get_spotify_access_token())
 }
 #' @export
+#' @rdname get_spotify
 get_west_spotify <- function() {
   spotifyr::get_artist("5K4W6rqBFWDnAN6FQUkS6x?si=1porkVNwRaeBFt-Q0WGHQw", authorization = spotifyr::get_spotify_access_token())
 }
