0ee359eacfd3145826a5b3d817e54d2d1ba2b147
Judith Bourque <59173478+judith-bourque@users.noreply.github.com>
2023-01-13 20:00:38 -0500

    Add get QID function

---
+++ R/wikidata.R
@@ -0,0 +1,15 @@
+#' Get QID for artists
+#'
+#' For internal purposes.
+#'
+#' @return Character vector of QID
+#' @export
+#'
+#' @examples
+#' get_qid()
+get_qid <- function(){
+  qid <- c("Q26876", # Taylor Swift
+           "Q15935" # Kanye West
+           )
+  qid
+}
